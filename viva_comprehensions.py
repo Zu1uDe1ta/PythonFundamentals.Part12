d15f67abee30ed75e1599102947d5f138febfd1a
Zu1u De1ta <C.Chavez@me.com>
2020-03-24 21:31:52 -0400

    this time with docstrings

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -9,36 +9,28 @@ class Parity(enum.Enum):
 
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
-    :param start:
-    :param stop:
-    :param parity:
-    :return:
+    :param start: starting value (inclusive)
+    :param stop: ending value (excluesive)
+    :param parity: strategy to be applied   
+    :return: returns a list with strategy implemented
     """
-    pass
+    # parity = lambda: x % 2 != 0
+    return [x for x in range(start, stop) if x % 2 != parity.value]
 
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
-    :param start:
-    :param stop:
-    :param strategy:
-    :return:
+    :param start: starting value (inclusive)
+    :param stop: ending value (excluesive)
+    :param strategy: strategy to be applied 
+    :return: returns a dict with strategy implemented
     """
-    pass
+    return {x: strategy(x) for x in range(start, stop)}
 
 
 def gen_set(val_in: str) -> Set:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
-    :param val_in:
-    :return:
+    :param val_in: takes in values
+    :return: returns the corresponding values of the str in lower case
     """
-    pass
+    return {c.upper() for c in val_in if c == c.lower()}
